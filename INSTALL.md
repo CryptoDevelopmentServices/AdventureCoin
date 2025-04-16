8878f3c5e086c7eb2375cef82d744b9e0305d134
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-04-16 15:19:31 +0100

    purge

--- INSTALL.md
+++
@@ -1,5 +0,0 @@
-Building Adventurecoin
-=============
-
-See doc/build-*.md for instructions on building the various
-elements of the Adventurecoin Core reference implementation of Adventurecoin.
--- contrib/auto_gdb/advc_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.advccore/advcd.pid file instead
-advc_pid=$(<~/.advccore/testnet3/advcd.pid)
-sudo gdb -batch -ex "source debug.gdb" advcd ${advc_pid}
