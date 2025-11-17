afe2b05d42ee79c0e1ebc840cf7d825a49b88075
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-11-17 22:03:26 +0000

    test 1

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,25 +1,42 @@
 package=boost
-$(package)_version=1_64_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.64.0/source/
+$(package)_version=1_74_0
+$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332
+$(package)_sha256_hash=83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
 $(package)_config_opts_debug=variant=debug
 $(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
+
+# Per-platform build options
 $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
-$(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
+$(package)_config_opts_darwin=threadapi=pthread runtime-link=shared
 $(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win32 runtime-link=static
 $(package)_config_opts_x86_64_mingw32=address-model=64
 $(package)_config_opts_i686_mingw32=address-model=32
 $(package)_config_opts_i686_linux=address-model=32 architecture=x86
-$(package)_toolset_$(host_os)=gcc
-$(package)_archiver_$(host_os)=$($(package)_ar)
-$(package)_toolset_darwin=darwin
+
+# Toolsets and archivers for each host OS
+$(package)_toolset_linux=gcc
+boost_toolset_linux=$($(package)_toolset_linux)
+$(package)_archiver_linux=$($(package)_ar)
+boost_archiver_linux=$($(package)_archiver_linux)
+
+$(package)_toolset_darwin=clang
+boost_toolset_darwin=$($(package)_toolset_darwin)
 $(package)_archiver_darwin=$($(package)_libtool)
+boost_archiver_darwin=$($(package)_archiver_darwin)
+
+$(package)_toolset_mingw32=gcc
+boost_toolset_mingw32=$($(package)_toolset_mingw32)
+$(package)_archiver_mingw32=$($(package)_ar)
+boost_archiver_mingw32=$($(package)_archiver_mingw32)
+
+# Same libs as before (you *can* drop "test" if you want)
 $(package)_config_libraries=chrono,filesystem,program_options,system,thread,test
+
 $(package)_cxxflags=-std=c++11 -fvisibility=hidden
 $(package)_cxxflags_linux=-fPIC
 endef
