58c0e23138ccf00edbb1e182176a800575cac448
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-11-19 22:36:21 +0000

    test 5

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -3,11 +3,12 @@ OSX_SDK_VERSION=10.11
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
 LD64_VERSION=253.9
 
-clang_prog=$(build_prefix)/bin/clang
-clangxx_prog=$(clang_prog)++
+# Correct cross-compiler prefix
+darwin_CC=$(host)-clang -mmacosx-version-min=$(OSX_MIN_VERSION) \
+          --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 
-darwin_CC=$(build_prefix)/bin/clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
-darwin_CXX=$(clang_prog)++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
+darwin_CXX=$(host)-clang++ -mmacosx-version-min=$(OSX_MIN_VERSION) \
+           --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
