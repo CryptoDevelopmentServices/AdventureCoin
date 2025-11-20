fe94078a206741e48f99b6e6c573b9552e1291e2
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-11-20 13:11:05 +0000

    Update libevent.mk

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,37 +1,26 @@
 package=libevent
 $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
-$(package)_file_name=libevent-$($(package)_version).tar.gz
+$(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
+# $(package)_patches = glibc_compatibility.patch
 
 define $(package)_preprocess_cmds
   ./autogen.sh
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts= \
-    --disable-shared \
-    --disable-openssl \
-    --disable-libevent-regress \
-    --disable-samples \
-    --enable-static
-
+  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
-
-  # macOS: DO NOT override CC/CXX — let depends toolchain control it
-  $(package)_config_opts_darwin= \
-    ac_cv_func_realloc_0_nonnull=yes \
-    ac_cv_func_malloc_0_nonnull=yes
 endef
 
+# define $(package)_preprocess_cmds
+#   patch -p1 -i $($(package)_patch_dir)/glibc_compatibility.patch
+# endef
+
 define $(package)_config_cmds
-  ./configure \
-    --host=$(host) \
-    --build=$(build) \
-    --prefix=$($(package)_staging_prefix_dir) \
-    $($(package)_config_opts) \
-    $($(package)_config_opts_$(host_os))
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
@@ -40,4 +29,7 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
 endef
