83ebd19ea05fba2e1b35ea540ef85a104144b643
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-11-20 18:39:38 +0000

    Update darwin.mk

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,13 +1,20 @@
-OSX_MIN_VERSION=10.8
+OSX_MIN_VERSION=10.11
 OSX_SDK_VERSION=10.11
 OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
-LD64_VERSION=253.9
 
-darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
-    --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
+# Must match ld64 from Xcode 7.x (macOS 10.11)
+LD64_VERSION=253.3
 
-darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
-    --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
+darwin_CC=clang -target $(host) \
+    -mmacosx-version-min=$(OSX_MIN_VERSION) \
+    --sysroot $(OSX_SDK) \
+    -mlinker-version=$(LD64_VERSION)
+
+darwin_CXX=clang++ -target $(host) \
+    -mmacosx-version-min=$(OSX_MIN_VERSION) \
+    --sysroot $(OSX_SDK) \
+    -mlinker-version=$(LD64_VERSION) \
+    -stdlib=libc++
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
