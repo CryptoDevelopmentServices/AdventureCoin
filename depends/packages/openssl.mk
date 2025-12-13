ed24607fd9c26d2df77b7f68fc294eff1d63b7a8
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-12-13 21:38:28 +0000

    openssl patch

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,8 +1,10 @@
 package=openssl
-$(package)_version=1.0.2t
-$(package)_download_path=http://slackware.cs.utah.edu/pub/openssl-ftp/source/old/1.0.2/
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=14cb464efe7ac6b54799b34456bd69558a749a4931ecfd9cf9f71d7881cac7bc
+$(package)_version=1.0.2
+$(package)_version_suffix=u
+$(package)_download_path=https://www.openssl.org/source/old/$($(package)_version)
+$(package)_file_name=$(package)-$($(package)_version)$($(package)_version_suffix).tar.gz
+$(package)_sha256_hash=ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f89669d16
+$(package)_patches=secure_getenv.patch
 
 define $(package)_set_vars
 $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
@@ -47,7 +49,6 @@ $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
 $(package)_config_opts_i686_linux=linux-generic32
 $(package)_config_opts_arm_linux=linux-generic32
-$(package)_config_opts_armv7l_linux=linux-generic32
 $(package)_config_opts_aarch64_linux=linux-generic64
 $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
@@ -58,12 +59,14 @@ $(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/secure_getenv.patch && \
   sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
   sed -i.old "s|engines apps test|engines|" Makefile.org
 endef
 
 define $(package)_config_cmds
-  ./Configure $($(package)_config_opts) && $(MAKE) depend
+  ./Configure $($(package)_config_opts) && \
+  make depend
 endef
 
 define $(package)_build_cmds
