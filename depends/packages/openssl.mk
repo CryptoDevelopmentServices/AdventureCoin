d9d74c30b36621dcaba89a52e39649a44bf116ce
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-12-14 23:23:48 +0000

    openssl patch 15

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -4,7 +4,6 @@ $(package)_version_suffix=u
 $(package)_download_path=https://www.openssl.org/source/old/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version)$($(package)_version_suffix).tar.gz
 $(package)_sha256_hash=ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f89669d16
-
 # PATCHES
 $(package)_patches += secure_getenv.patch
 
@@ -47,7 +46,6 @@ $(package)_config_opts+=no-whirlpool
 $(package)_config_opts+=no-zlib
 $(package)_config_opts+=no-zlib-dynamic
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
-
 $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
 $(package)_config_opts_i686_linux=linux-generic32
@@ -56,37 +54,27 @@ $(package)_config_opts_aarch64_linux=linux-generic64
 $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
-
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
 endef
 
-
-# ======================
-# PREPROCESS
-# ======================
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/secure_getenv.patch && \
   sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
   sed -i.old "s|engines apps test|engines|" Makefile.org
 endef
 
-
-# ======================
-# CONFIGURE (REAL FIX HERE)
-# ======================
 define $(package)_config_cmds
   ./Configure $($(package)_config_opts) && \
-  echo "Patching final util/domd to remove clang..." && \
-  sed -i.bak 's/clang/cc/g' util/domd && \
+  echo "Patching util/domd to force GCC-based dependencies..." && \
+  sed -i.bak \
+    -e 's|MAKEDEPEND=.*|MAKEDEPEND="$$CC -MM"|' \
+    -e 's|^\( *\)makedepend|\1$$CC -MM|' \
+    util/domd && \
   make depend
 endef
 
-
-# ======================
-# BUILD/STAGE
-# ======================
 define $(package)_build_cmds
   $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
 endef
